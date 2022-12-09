68923500c051bf114d860176c05f9c9f3f741b9b
Fred Murigi <105373496+Freddie16@users.noreply.github.com>
2022-12-09 09:50:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: "Fred",
+        email: "fred@example.com",
+        
+})
+}
+
+fetch("http://localhost:3000/users",submitData)
+   .then(res => res.json())
+   .then(data => {
+    console.log(data)
+   })
+   .catch(error => console.log(error))
